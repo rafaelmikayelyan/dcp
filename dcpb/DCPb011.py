cd9190923e68820febcc040f8a56424632caca35
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2021-02-15 12:16:56 -0800

    test(DCPb011): correct tests

--- dcpb/DCPb011.py
+++ dcpb/DCPb011.py
@@ -46,28 +46,28 @@ def check_array(array):
 
 class Test(unittest.TestCase):
     def test_array_wrong_input_str(self):
-        self.assertFalse(dcpb001("x"), "should be an array of #'s size=2+")
+        self.assertFalse(dcpb011("x"), "should be an array of #'s size=2+")
 
     def test_array_wrong_input_int(self):
-        self.assertFalse(dcpb001(1), "should be an array of #'s size=2+")
+        self.assertFalse(dcpb011(1), "should be an array of #'s size=2+")
 
     def test_array_with_string(self):
-        self.assertFalse(dcpb001([1, "x"]), "should be an array of #'s size=2+")
+        self.assertFalse(dcpb011([1, "x"]), "should be an array of #'s size=2+")
 
     def test_array_of_0(self):
-        self.assertFalse(dcpb001([]), "should be an array of #'s size=2+")
+        self.assertFalse(dcpb011([]), "should be an array of #'s size=2+")
 
     def test_array_of_1(self):
-        self.assertFalse(dcpb001([3]), "should be an array of #'s size=2+")
+        self.assertFalse(dcpb011([3]), "should be an array of #'s size=2+")
 
     def test_array_of_2(self):
-        self.assertEqual(dcpb001([1, 2]), [2, 1], "should be [2, 1]")
+        self.assertEqual(dcpb011([1, 2]), [2, 1], "should be [2, 1]")
 
     def test_array_of_3(self):
-        self.assertEqual(dcpb001([2, 3, 4]), [12, 8, 6], "should be [12, 8, 6]")
+        self.assertEqual(dcpb011([2, 3, 4]), [12, 8, 6], "should be [12, 8, 6]")
 
     def test_array_of_5(self):
-        self.assertEqual(dcpb001([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24], "should be [120, 60, 40, 30, 24]")
+        self.assertEqual(dcpb011([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24], "should be [120, 60, 40, 30, 24]")
 
 
 if __name__ == '__main__':
