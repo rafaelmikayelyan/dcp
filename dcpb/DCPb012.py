d744e2eb12498cbca195164debf8e4c4ea1dfe72
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2021-02-15 14:39:39 -0800

    sol(DCPb012): add solution

--- dcpb/DCPb012.py
+++ dcpb/DCPb012.py
@@ -6,11 +6,34 @@ import unittest
 
 
 def dcpb012(array):
-    pass
+    left = get_left(array)
+    return left, get_right(array, left)
+
+
+def get_left(array):
+    left = i = 0
+    while i < len(array):
+        if array[i] < array[left]:
+            return left
+        left = i
+        i += 1
+    return i
+
+
+def get_right(array, left):
+    array_length = len(array) - 1
+    right = array_length
+    i = 0
+    while i < len(array):
+        if (array[array_length - i] > array[right]) or (array[left] > array[right]):
+            return right
+        i += 1
+        right = (len(array) - i)
+    return -1
 
 
 class Test(unittest.TestCase):
-    def test_array_of_5(self):
+    def test_array_of_8(self):
         self.assertEqual(dcpb012([3, 7, 5, 6, 9]), (1, 3), "should be (1, 3)")
 
 
