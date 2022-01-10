f22ea6c02f6ef6dd057858c60a12f516715c9227
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-01-10 01:37:23 -0800

    sol(aoc_21_06): sol to p1 and p2

--- aoc/aoc_21/aoc_21_06/aoc_21_06.py
+++ aoc/aoc_21/aoc_21_06/aoc_21_06.py
@@ -5,15 +5,52 @@ def read_file(input):
     return data
 
 
+def split_input(input):
+    temp = []
+    output = []
+    temp = input[0].split()
+    for i in temp[0].split(','):
+        output.append(int(i))
+    return output
+
+
+def next_day(fish):
+    i = 0
+    newborn = []
+    while i < len(fish):
+        if fish[i] == 0:
+            fish[i] = 6
+            newborn.append(8)
+        else:
+            fish[i] -= 1
+        i += 1
+    if newborn:
+        fish.extend(newborn)
+    return fish
+
+
+def population_after(days, raw):
+    day = 0
+    while day < days:
+        print(day)
+        next_day(raw)
+        day += 1
+    return raw
+
+
 def run():
     # raw = read_file("input.txt")
-    sample = [3,4,3,1,2]
+    raw = [3,4,3,1,2] # Sample
 
-    answer_1 = False
+    population = raw
+    # population = split_input(raw)
+
+
+    answer_1 = len(population_after(80, population))
     print(f'AOC 2021-05-1: {answer_1}')
 
-    answer_2 = False
-    print(f'AOC 2021-05-2: {answer_2}')
+    # answer_2 = len(population_after(256, population))
+    # print(f'AOC 2021-05-2: {answer_2}')
 
 
 if __name__ == '__main__':
