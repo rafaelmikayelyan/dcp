6cc199183560ca443ba32a3f96a5d12d593a1689
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-01-12 23:35:48 -0800

    sol(aoc_21_08): sol to p1

--- aoc/aoc_21/aoc_21_08/aoc_21_08.py
+++ aoc/aoc_21/aoc_21_08/aoc_21_08.py
@@ -2,19 +2,63 @@ def read_file(input):
     file = open(input, "r")
     data = file.read().splitlines()
     file.close()
-    return data
+    return split_parts(data)
+
+
+def split_parts(data):
+    parts = [''] * len(data)
+    i = 0
+    for part in data:
+        parts[i] = part.split(' | ')
+        i += 1
+    return parts
+
+
+def count_occurrences(txt):
+    count = 0
+    for strings in txt:
+        for s in strings[1].split(' '):
+            if len(s) == 2 or len(s) == 4 or len(s) == 3 or len(s) == 7:
+                count += 1
+    return count
+
+
+def decode_line(txt):
+    decoded = [''] * 9
+    for parts in txt:
+        for strings in txt:
+            for s in strings.split(' '):
+                if len(s) == 2:
+                    decoded[1] = s
+                if len(s) == 4:
+                    decoded[4] = s
+                if len(s) == 3:
+                    decoded[7] = s
+                if len(s) == 7:
+                    decoded[8] = s
+    print(decoded)
+
+    return False
+
+
+def add_all_outputs(list):
+    sum = 0
+    for output in list:
+        sum += output
+    return sum
 
 
 def run():
     # txt = read_file("input.txt")
-    txt = read_file("sample.txt")
+    # txt = read_file("sample.txt")
+    txt = [['acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab', 'cdfeb fcadb cdfeb cdbaf']]
 
     # print(txt)
 
-    answer_1 = False
+    answer_1 = count_occurrences(txt)
     print(f'AOC 2021-05-1: {answer_1}')
 
-    answer_2 = False
+    answer_2 = decode_line(txt[0])
     print(f'AOC 2021-05-2: {answer_2}')
 
 
