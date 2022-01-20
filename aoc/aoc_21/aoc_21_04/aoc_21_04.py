ea7c15b4ea3808f76729f2c99e6a22ffb37b78b4
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-01-19 23:13:21 -0800

    fix(aoc_21_04): fix solutions

--- aoc/aoc_21/aoc_21_04/aoc_21_04.py
+++ aoc/aoc_21/aoc_21_04/aoc_21_04.py
@@ -14,24 +14,16 @@ def extract_random(raw):
 
 def extract_boards(raw):
     boards = []
-    score_boards = []
     board = []
-    score_board = []
-    row = []
-    score_row = []
 
     for raw_row in raw:
         if len(raw_row) > 1:
             row = row_sting_to_int(raw_row.split(' '))
-            score_row = [0] * len(row)
             board.append(row)
-            score_board.append(score_row)
             if len(board) == 5:
                 boards.append(board)
                 board = []
-                score_boards.append(score_board)
-                score_board = []
-    return boards, score_boards
+    return boards
 
 
 def row_sting_to_int(row):
@@ -46,13 +38,6 @@ def row_sting_to_int(row):
     return row
 
 
-def check_boards(score_boards):
-    answer = []
-    for score in score_boards:
-        answer = check_board(score)
-    return answer
-
-
 def check_board(board):
     sum_v = 0
     sum_h = 0
@@ -60,54 +45,98 @@ def check_board(board):
     for row in board:
         for i in row:
             sum_h = sum_h + i
-        if sum_h == 5:
+        if sum_h == -5:
             return board
         sum_h = 0
 
     for i in range(len(board)):
         for row in board:
             sum_v = sum_v + row[i]
-        if sum_v == 5:
+        if sum_v == -5:
             return board
         sum_v = 0
 
 
-def calculate_score(board, score, number):
+def calculate_score(board, number):
     sum_unmarked = 0
-    for row in score:
-        i = 0
-        while i < 5:
-            if row[i] == 0:
-                sum_unmarked = sum_unmarked + board[score.index(row)][i]
-            i = i + 1
+    for row in board:
+        for i in row:
+            if i != -1:
+                sum_unmarked = sum_unmarked + i
     return sum_unmarked * number
 
 
-def mark_numbers(randoms, boards, scores):
-    winning_board = []
+def mark_numbers(randoms, boards, last_hand):
+    board = 0
+    row = 0
+    number = 0
+    last_board = []
+    last_number = 0
+    winners = []
     for random in randoms:
-        for board in boards:
-            for row in board:
-                for number in row:
-                    if number == random:
-                        scores[boards.index(board)][board.index(row)][row.index(number)] = 1
-                        winning_board = check_boards(scores)
-                        if winning_board:
-                            return winning_board, random
+        while board < len(boards):
+            if board not in winners:
+                while row < len(boards[board]):
+                    while number < len(boards[board][row]):
+                        if boards[board][row][number] == random:
+                            boards[board][row][number] = -1
+                            winning_board = check_board(boards[board])
+                            if winning_board:
+                                if last_hand:
+                                    winners.append(board)
+                                    last_board = winning_board
+                                    last_number = random
+                                else:
+                                    return winning_board, random
+                        number += 1
+                    number = 0
+                    row += 1
+            row = 0
+            board += 1
+        board = 0
+    return last_board, last_number
+
+
+def print_boards(boards):
+    for board in boards:
+        i = boards.index(board)
+        print(f' - {i} ------------------- {i} - ')
+        print_board(board)
+        print()
+
+
+def print_board(board):
+    for row in board:
+        print_row(row)
+
+
+def print_row(row):
+    output = ""
+    for i in row:
+        if i == -1:
+            output += "  ."
+        elif i < 10:
+            output += "  " + str(i)
+        else:
+            output += " " + str(i)
+    print(output)
 
 
 def run():
     raw = read_file("aoc_21_04_input.txt")
     # raw = read_file("sample.txt")
+
     randoms = extract_random(raw)
-    boards, scores = extract_boards(raw)
+    boards = extract_boards(raw)
 
-    winning_board, last_number = mark_numbers(randoms, boards, scores)
+    winning_board, last_number = mark_numbers(randoms, boards, False)
 
-    answer_1 = calculate_score(boards[scores.index(winning_board)], winning_board, last_number)
+    answer_1 = calculate_score(winning_board, last_number)
     print(f'AOC 2021-04-1: {answer_1}')
 
-    answer_2 = False
+    last_hand, last_number = mark_numbers(randoms, boards, True)
+
+    answer_2 = calculate_score(last_hand, last_number)
     print(f'AOC 2021-04-2: {answer_2}')
 
 
