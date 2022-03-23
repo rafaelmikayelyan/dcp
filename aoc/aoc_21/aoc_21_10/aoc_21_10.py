ff1f1868f8686d1564e7dfdf281d3959d60a7d9f
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-03-22 23:51:33 -0700

    sol(aoc_21_10): solution to p1

--- aoc/aoc_21/aoc_21_10/aoc_21_10.py
+++ aoc/aoc_21/aoc_21_10/aoc_21_10.py
@@ -1,3 +1,7 @@
+BRACKETS = {'(': ')', '[': ']', '{': '}', '<': '>'}
+POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137}
+
+
 def read_file(txt):
     file = open(txt, "r")
     data = file.read().splitlines()
@@ -5,11 +9,53 @@ def read_file(txt):
     return data
 
 
-def run():
-    # txt = read_file("input.txt")
-    txt = read_file("sample.txt")
+def check_first_illegal(line):
+    opened = []
+    for i in line:
+        if is_opening(i):
+            opened.append(i)
+        if is_closing(i):
+            if not is_complete(opened.pop(), i):
+                return i
 
-    answer_1 = False
+
+def find_all_first_illegal(txt):
+    illegal_list = []
+    for i in txt:
+        illegal = check_first_illegal(i)
+        if illegal:
+            illegal_list.append(illegal)
+    return illegal_list
+
+
+def is_opening(i):
+    return i in BRACKETS
+
+
+def is_closing(i):
+    return i in BRACKETS.values()
+
+
+def is_complete(opening, closing):
+    return BRACKETS[opening] == closing
+
+
+def score_illegal(illegal):
+    score = 0
+    print(illegal)
+    if len(illegal) > 0:
+        for i in illegal:
+            score += POINTS[i]
+    return score
+
+
+def run():
+    txt = read_file("input.txt")
+    # txt = read_file("sample.txt")
+
+    illegal_list = find_all_first_illegal(txt)
+
+    answer_1 = score_illegal(illegal_list)
     print(f'AOC 2021-09-1: {answer_1}')
 
     answer_2 = False
