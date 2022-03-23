3d1924e5f027d9b24a1fba0ba94764e8c5bab57c
rafaelmikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-03-23 00:47:53 -0700

    sol(aoc_21_10): solve p2

--- aoc/aoc_21/aoc_21_10/aoc_21_10.py
+++ aoc/aoc_21/aoc_21_10/aoc_21_10.py
@@ -1,5 +1,6 @@
 BRACKETS = {'(': ')', '[': ']', '{': '}', '<': '>'}
-POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137}
+POINTS_CORRUPTED = {')': 3, ']': 57, '}': 1197, '>': 25137}
+POINTS_INCOMPLETE = {')': 1, ']': 2, '}': 3, '>': 4}
 
 
 def read_file(txt):
@@ -9,23 +10,28 @@ def read_file(txt):
     return data
 
 
-def check_first_illegal(line):
+def check_line_illegal(line):
     opened = []
     for i in line:
         if is_opening(i):
             opened.append(i)
         if is_closing(i):
             if not is_complete(opened.pop(), i):
-                return i
+                return {"corrupted": i}
+    if opened:
+        return {"incomplete": opened}
 
 
-def find_all_first_illegal(txt):
-    illegal_list = []
+def find_all_illegal(txt):
+    list_corrupted = []
+    list_incomplete = []
     for i in txt:
-        illegal = check_first_illegal(i)
-        if illegal:
-            illegal_list.append(illegal)
-    return illegal_list
+        illegal = check_line_illegal(i)
+        if "corrupted" in illegal:
+            list_corrupted.append(illegal["corrupted"])
+        else:
+            list_incomplete.append(illegal["incomplete"])
+    return list_corrupted, list_incomplete
 
 
 def is_opening(i):
@@ -40,25 +46,45 @@ def is_complete(opening, closing):
     return BRACKETS[opening] == closing
 
 
-def score_illegal(illegal):
+def score_corrupted(corrupted):
     score = 0
-    print(illegal)
-    if len(illegal) > 0:
-        for i in illegal:
-            score += POINTS[i]
+    if len(corrupted) > 0:
+        for i in corrupted:
+            score += POINTS_CORRUPTED[i]
     return score
+
+
+def value_of_incomplete_bracket(bracket):
+    return POINTS_INCOMPLETE[BRACKETS[bracket]]
+
+
+def score_incomplete_line(incomplete):
+    score = 0
+    incomplete.reverse()
+    for i in incomplete:
+        score = score * 5 + value_of_incomplete_bracket(i)
+    return score
+
+
+def score_incomplete_list(incomplete):
+    score = []
+    for i in incomplete:
+        score.append(score_incomplete_line(i))
+    score.sort()
+    index_mid = int(len(score)/2)
+    return score[index_mid]
 
 
 def run():
     txt = read_file("input.txt")
     # txt = read_file("sample.txt")
 
-    illegal_list = find_all_first_illegal(txt)
+    list_corrupted, list_incomplete = find_all_illegal(txt)
 
-    answer_1 = score_illegal(illegal_list)
+    answer_1 = score_corrupted(list_corrupted)
     print(f'AOC 2021-09-1: {answer_1}')
 
-    answer_2 = False
+    answer_2 = score_incomplete_list(list_incomplete)
     print(f'AOC 2021-09-2: {answer_2}')
 
 
