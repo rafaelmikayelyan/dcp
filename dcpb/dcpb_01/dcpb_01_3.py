dc314eb2c0a2474a2fb97e2687cfd73a488a1a35
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-10-02 00:19:47 -0700

    fix(dcpb_01_03): fix solution

--- dcpb/dcpb_01/dcpb_01_3.py
+++ dcpb/dcpb_01/dcpb_01_3.py
@@ -7,17 +7,25 @@ import unittest
 
 
 def dcpb013(array):
-    return False
+    max_sum = 0
+    for i in range(len(array) - 1):
+        for j in range(i, len(array) + 1):
+            max_sum = max(max_sum, sum(array[i:j]))
+    return max_sum
 
 
 class Test(unittest.TestCase):
-    def test_array_sum(self):
-        self.assertEqual(dcpb013([34, -50, 42, 14, -5, 86]), (137, [42, 14, -5, 86]),
-                         "should be 137 from [42, 14, -5, 86]")
+    def test_array_sum_137(self):
+        self.assertEqual(dcpb013([34, -50, 42, 14, -5, 86]), 137,
+                         "137 from [42, 14, -5, 86]")
+
+    def test_array_sum_0(self):
+        self.assertEqual(dcpb013([-34, -50, -42, -14, -5, -86]), 0,
+                         "0 from []")
 
     # def test_extra_array(self):
-    #     self.assertEqual(dcpb013_extra([8, -1, 3, 4]), (15, [3, 4, 8]),
-    #                      "should be 137 from [42, 14, -5, 86]")
+    #     self.assertEqual(dcpb013_extra([8, -1, 3, 4]), 15,
+    #                      "15 from [3, 4, 8]")
 
 
 if __name__ == '__main__':
