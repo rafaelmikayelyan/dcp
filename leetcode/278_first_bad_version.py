375a1bcfadcaa27b3e119e61772a607837d3517d
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-12-10 00:04:46 -0800

    add(leetcode_278): initial upload

---
+++ leetcode/278_first_bad_version.py
@@ -0,0 +1,24 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if n < 2:
+            if isBadVersion(n):
+                return n
+        
+        low = 1
+        high = n
+        output = n
+
+        while not low > high :
+            mid = math.floor((low + high) / 2)
+
+            if isBadVersion(mid):
+                output = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return output
+
+            
